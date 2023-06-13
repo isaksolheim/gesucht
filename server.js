549d500798b28b7e6df0bffd6102fa0ec4757e6a
isaksolheim <mail@isak.me>
2023-06-13 20:12:42 +0200

    Add dockerfile

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const lib = require("./lib");
 app.use(express.json());
 
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
 app.post("/webhook", async (req, res) => {
   await lib.tryToSendMessage();
   res.sendStatus(200);
@@ -12,6 +15,7 @@ app.get("/", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
+
